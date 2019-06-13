ccc04805e974daf7ea25348ae490876e9b6564ab
David Riley <driley@platformscience.com>
2019-06-12 20:41:24 -0700

    use data provider in test

--- tests/Transformer/WeatherTimeTransformerTest.php
+++ tests/Transformer/WeatherTimeTransformerTest.php
@@ -8,59 +8,73 @@ use stdClass;
 
 class WeatherTimeTransformerTest extends TestCase
 {
-    public function testValidWeatherForZip()
+    /**
+     * @dataProvider validWeatherForZipProvider
+     */
+    public function testValidWeatherForZip($validZip, $validWeatherForZip)
     {
-        $validZip = "92109";
-        $validWeatherForZip = json_decode(json_encode([
-            "coord" => [
-                "lon" => "-117.21",
-                "lat" => "32.78",
-            ],
-            "weather" => [
-                "0" => [
-                    "id" => "804",
-                    "main" => "Clouds",
-                    "description" => "overcast clouds",
-                    "icon" => "04d",
-                ],
-            ],
-            "base" => "stations",
-            "main" => [
-                "temp" => "69.3",
-                "pressure" => "1014",
-                "humidity" => "68",
-                "temp_min" => "62.01",
-                "temp_max" => "75.99",
-            ],
-            "visibility" => "14484",
-            "wind" => [
-                "speed" => "8.05",
-                "deg" => "260",
-            ],
-            "clouds" => [
-                "all" => "90",
-            ],
-            "dt" => "1559937065",
-            "sys" => [
-                "type" => "1",
-                "id" => "5026",
-                "message" => "0.0096",
-                "country" => "US",
-                "sunrise" => "1559911241",
-                "sunset" => "1559962499",
-            ],
-            "timezone" => "-25200",
-            "id" => "420008330",
-            "name" => "San Diego",
-            "cod" => "200",
-        ]));
         $transformed = WeatherTimeTransformer::transform(
             $validZip,
-            $validWeatherForZip
+            // TODO: find a better way to cast to stdClass
+            json_decode(json_encode($validWeatherForZip))
         );
         $this->assertArrayHasKey('location_data', $transformed);
         $this->assertArrayHasKey('general_weather', $transformed);
         $this->assertArrayHasKey('weather_reports', $transformed);
         $this->assertEquals($validZip, $transformed['zip']);
+    }
+
+    /*
+     * data provider for testValidWeatherForZip
+     */
+    public function validWeatherForZipProvider() {
+        return [
+            [
+                "92109",
+                [
+                    "coord" => [
+                        "lon" => "-117.21",
+                        "lat" => "32.78",
+                    ],
+                    "weather" => [
+                        "0" => [
+                            "id" => "804",
+                            "main" => "Clouds",
+                            "description" => "overcast clouds",
+                            "icon" => "04d",
+                        ],
+                    ],
+                    "base" => "stations",
+                    "main" => [
+                        "temp" => "69.3",
+                        "pressure" => "1014",
+                        "humidity" => "68",
+                        "temp_min" => "62.01",
+                        "temp_max" => "75.99",
+                    ],
+                    "visibility" => "14484",
+                    "wind" => [
+                        "speed" => "8.05",
+                        "deg" => "260",
+                    ],
+                    "clouds" => [
+                        "all" => "90",
+                    ],
+                    "dt" => "1559937065",
+                    "sys" => [
+                        "type" => "1",
+                        "id" => "5026",
+                        "message" => "0.0096",
+                        "country" => "US",
+                        "sunrise" => "1559911241",
+                        "sunset" => "1559962499",
+                    ],
+                    "timezone" => "-25200",
+                    "id" => "420008330",
+                    "name" => "San Diego",
+                    "cod" => "200",
+                ]
+            ]
+        ];
     }
 }
